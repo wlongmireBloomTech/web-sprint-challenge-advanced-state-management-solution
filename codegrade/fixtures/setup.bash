ab1dfa000cc7b64112dc95c09202937d81f5b37f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-02-09 11:04:10 -0500

    adds mutation shim and changes to setup.bash in fixtures.

--- codegrade/fixtures/setup.bash
+++ codegrade/fixtures/setup.bash
@@ -5,5 +5,5 @@ shopt -s extglob
 rm -rf "$STUDENT/src/codegrade" || true
 mkdir "$STUDENT/src/codegrade"
 mv "$FIXTURES/setupTests.js" "$STUDENT/src";
-mv "$FIXTURES/tests.test.js" "$STUDENT/src/codegrade"
-mv "$FIXTURES/{jest.config.js}" "$STUDENT"
+mv "$FIXTURES/codegrade_mvp.test.js" "$STUDENT/src/codegrade"
+mv "$FIXTURES/jest.config.js" "$STUDENT"
